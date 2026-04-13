c8bbf2807f38809acc5070895087c93b44b30d98
pratiksha950 <salunkepratiksha950@gmail.com>
2026-04-13 16:48:29 +0530

    required changes added:

---
+++ frontend/src/landing_page/OpenAccount.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function OpenAccount() {
+  return (
+    <div>
+      OpenAccount &nbsp;
+
+
+      <i className="fa-solid fa-user-plus"></i>
+
+    </div>
+  )
+}
+
+export default OpenAccount
--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import HeroImage from '../../assets/images/homeHero.png'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div className='container'>
+        <div className='row'>
+            <img 
+              src={HeroImage} 
+              alt='Hero Image' 
+              className='img-fluid' 
+            />
+        </div>
+
+        <h1>Hero</h1>
+    </div>
   )
 }
 
