920ef7f25a74cd22e4b7c0e33f5df35df29d906f
pratiksha950 <salunkepratiksha950@gmail.com>
2026-04-15 20:39:19 +0530

    product section addded:

--- frontend/src/landing_page/products/Product.jsx
+++ frontend/src/landing_page/products/Product.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Hero from './Hero'
+import LeftSection from './LeftSection'
+import kite from '../../assets/images/kite.png'
 
 function Product() {
   return (
-    <div>Product</div>
+    <div>
+      <Hero />
+<LeftSection 
+  imageSrc={kite}
+  title="Kite"
+  description="Our ultra-fast flagship trading platform with streaming market data, advanced charts, and an elegant UI. Enjoy the Kite experience seamlessly on your Android and iOS devices."
+  trydemo="#"
+  learnmore="#"
+/>    </div>
   )
 }
 
 export default Product
+
+
+
