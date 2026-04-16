93f052f2b54b56350c2798371a2e40e2d95e17fc
pratiksha950 <salunkepratiksha950@gmail.com>
2026-04-16 11:00:37 +0530

    product page completed

--- frontend/src/landing_page/pricing/PricingPage.jsx
+++ frontend/src/landing_page/pricing/PricingPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './Hero'
 import Brokerage from './Brokerage'
+import Charge from './Charge'
 
 function PricingPage() {
   return (
     <div>PricingPage
       <Hero />
       <Brokerage />
+      <Charge />
     </div>
   )
 }
