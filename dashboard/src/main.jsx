b3bd660b72b5bbf758c235dd96608d26358b2888
pratiksha950 <salunkepratiksha950@gmail.com>
2026-04-23 09:59:20 +0530

    error resolved

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../src/components/Home.jsx"; 
